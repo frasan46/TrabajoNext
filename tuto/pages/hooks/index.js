8430915e5cb68c0488f3fc8ac9a2696c92209ff6
frasan46 <franschez98@gmail.com>
2021-11-24 18:46:38 +0100

    a

--- tuto/pages/hooks/index.js
+++ tuto/pages/hooks/index.js
@@ -1,31 +1,4 @@
-/*import React, {useEffect, userEffect, useState} from 'react'
 
-export default function useEffectPage(){
-    
-    const [items, setItems] = useState([]);
-    const [resourceType, setResourceType] = useState("Posts");
-    useEffect(()=> {
-        fetch('https://pokeapi.co/api/v2/pokemon/${resourceType}')
-        .then(response => response.json())
-        .then(json => setItems(json))
-
-    
-        
-    },[resourceType]);
-
-    return <>
-    <button onClick={ () => setResourceType("Posts")}>k</button>
-    <br></br>
-    <button onClick={ () => setResourceType("a")}>aaaaaa</button>
-    
-    {items.map((item, index) => {
-                    return <li key={index} className="my-10 bg-gray-400">{JSON.stringify(item)}</li>
-                })}
-    </>
-
-    
-}
-*/
 
 
 
@@ -43,13 +16,13 @@ export default function useEffectPage() {
     const handleResize = () => {
      
         
-        //setItems([])
+        setItems([])
 
     }
 
    
     useEffect(() => {
-       // window.addEventListener('blur', handleResize);
+       window.addEventListener('blur', handleResize);
         
         console.log("d")
         resourceType -= 1;
