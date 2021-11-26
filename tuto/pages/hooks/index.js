853c33982eff6066c05ddde223beceff26e1c0ca
frasan46 <franschez98@gmail.com>
2021-11-26 02:41:55 +0100

    actualizacion UwU

--- tuto/pages/hooks/index.js
+++ tuto/pages/hooks/index.js
@@ -12,6 +12,8 @@ export default function useEffectPage() {
     const [numero, cantidad] = useState('1');
 
     const [items, setItems] = useState([]);
+    const [NumeroFocus, setfocus] = useState(0);
+    const [img, setimg] = useState("");
 
     const handleResize = () => {
      
@@ -22,19 +24,20 @@ export default function useEffectPage() {
 
    
     useEffect(() => {
-       window.addEventListener('blur', handleResize);
+      // window.addEventListener('blur', handleResize);
         
         console.log("d")
         resourceType -= 1;
         fetch(`https://pokeapi.co/api/v2/pokemon?limit=${numero}&offset=${resourceType}`)
+
             .then(response => response.json())
             .then(json => setItems(json))
-            
-            
-    }, [resourceType] , [numero]);
 
- 
-
+            setimg( `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${NumeroFocus}.png`)
+            console.log(img)
+    }, [resourceType , numero , NumeroFocus]);
+   
+   
     const ComponentUE = dynamic(() => import('../../components/dinamic/useEffectComp'), {
         ssr: false,
       });
@@ -43,17 +46,22 @@ export default function useEffectPage() {
 
     let objeto =  items["results"]
     let nombrePochimon= [];
+    let listasmagenes= []
+    console.log(items)
     for (const i in objeto) {
-        nombrePochimon.push(objeto[i]["name"]);
-
+        nombrePochimon.push(objeto[i]);
+        
       }
+      
     
 
 
 
     return (
         <> 
+        
         <h1> Poke APIacidos</h1>
+        <div id="al">
         <div id="div" className= "">
            <form>
                 numero de la pokedex: 
@@ -63,13 +71,18 @@ export default function useEffectPage() {
                
               
            </form>
+           <img src={img}></img>
+           </div>
+           <div id="img">
+          
+             
            </div>
            <div id="respuesta">
            {nombrePochimon.map((post) => (
-        <p>{cont++} {post}</p>
+        <p  > <button onClick={(e) => setfocus(e.target.value)} value={cont}>+</button>{cont++} {post["name"]}</p>
       ))}
       </div>
-       
+      </div>
         </>
 
     )
